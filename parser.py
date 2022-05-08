572d7f89da0ef0208a801278092168ec51d713c7
Kostia2004 <k.a.kudriavsev@mail.ru>
2022-05-08 19:38:57 +0300

    get request

--- parser.py
+++ parser.py
@@ -5,3 +5,8 @@ import pandas as pd
 # Задача - спарсить данные о сдаваемых в Москве квартирах в таблицу
 # 1) Спарсить ссылки на страницы о квартирах со страницы поиска по фильтрам
 # 2) Спарсить данные по ссылкам
+
+params = {'deal_type': 'rent', 'engine_version':'2', 'is_by_homeowner':'1', 'offer_type':'flat', 'region':'1', 'room1':'1', 'room2':'1', 'room3':'1', 'room4':'1', 'room5':'1', 'room6':'1', 'room7':'1', 'room9':'1', 'type':'4'}
+
+request = requests.get('https://www.cian.ru/cat.php', params=params)
+
